a8d6169e6123e41a36aa587595ad4e424ee88986
songa1 <songachillethe1her@gmail.com>
2020-12-07 08:31:25 +0200

    I added secret to my environment variable

--- configAuth/authMiddleWare.js
+++ configAuth/authMiddleWare.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const requireAuth = (req, res, next)=>{
     const token = req.cookies.jwt;
     if(token){
-        jwt.verify(token, 'AISGMSchilleranziongaanzayecret', (err, decodedToken)=>{
+        jwt.verify(token, process.env.SECRET, (err, decodedToken)=>{
             if(err){
                 console.log(err);
                 res.redirect('/login');
